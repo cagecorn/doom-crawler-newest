aff687d5ba521f6a140633780881de3b9ba7520f
cagecorn <lmcbv@naver.com>
2025-06-23 05:56:11 +0900

    Update equipmentRenderManager.js

--- src/managers/equipmentRenderManager.js
+++ src/managers/equipmentRenderManager.js
@@ -14,8 +14,8 @@ export class EquipmentRenderManager {
 
         const drawWidth = entity.width * 0.8;
         const drawHeight = entity.height * 0.8;
-        const drawX = entity.width * 0.1;
-        const drawY = entity.height * 0.1;
+        const drawX = entity.width * -0.2;
+        const drawY = entity.height * -1;
 
         ctx.drawImage(weapon.image, drawX, drawY, drawWidth, drawHeight);
     }
