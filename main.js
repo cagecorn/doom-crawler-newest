eb962c65c76750c10715e23a5b13396fc94f9084
cagecorn <lmcbv@naver.com>
2025-06-26 00:57:21 +0900

    Revert "Add game canvas and fix startup"

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ import { registerServiceWorker } from './src/utils/swRegister.js';
 
 let game = null;
 
+function initializeAudio() {
+    if (game) {
+        game.startBGM();
+    }
+}
+
 window.onload = () => {
     registerServiceWorker();
-    const canvas = document.getElementById('gameCanvas');
-    game = new Game(canvas);
+    game = new Game();
     game.start();
+    document.addEventListener('keydown', initializeAudio, { once: true });
+    document.addEventListener('click', initializeAudio, { once: true });
 };
